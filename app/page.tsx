d510c3bb49dc57c9fdaf86c804b5a33e515450ec
9riousen <9riousen@gmail.com>
2024-01-19 01:35:42 +0900

    첫 커밋

--- app/page.tsx
+++ app/page.tsx
@@ -1,111 +1,69 @@
 import Image from 'next/image'
 
-export default function Home() {
+// https://vos.line-scdn.net/landpress-content-v2-ogautzqf79ax91l8h8icywfr/1705378299002.pdf?updatedAt=1705378299000
+const KLAY_TO_FNSA_RATIO = 148.079656;
+
+async function fetchKrwByTicker(ticker: string) {
+  const res = await fetch(`https://api.bithumb.com/public/ticker/${ticker}_KRW`);
+  const parsedJson = await res.json();
+  const krw: string = parsedJson['data']['closing_price'];
+  return krw;
+}
+
+export default async function Home() {
+  const krwFnsa = Number.parseInt(await fetchKrwByTicker('FNSA'));
+  const krwKlay = Number.parseInt(await fetchKrwByTicker('KLAY'));
+  const currentRate = krwFnsa / krwKlay;
+  const formatKrw = new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' });
+  const formatRate = new Intl.NumberFormat('ko-KR', { maximumFractionDigits: 3 })
+  const LOGO_SIZE = 128;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center p-24 bg-[#0fc9e6]">
+      <div className="relative flex place-items-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-col items-center">
+          <Image
+            src='/klaytn.png'
+            width={LOGO_SIZE}
+            height={LOGO_SIZE}
+            alt='Klaytn' />
+          <div className="px-6 py-2">
+            <div className="text-center font-bold text-xl">클레이(KLAY)</div>
+            <p className="text-center text-gray-700 text-base">{formatKrw.format(krwKlay)}</p>
+            <p className="text-center bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">1</p>
+          </div>
+        </div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-col items-center">
+          <Image
+            src='/finschia.png'
+            width={LOGO_SIZE}
+            height={LOGO_SIZE}
+            alt='Finschia' />
+          <div className="px-6 py-2">
+            <div className="text-center font-bold text-xl">핀시아(FNSA)</div>
+            <p className="text-center text-gray-700 text-base">{formatKrw.format(krwFnsa)}</p>
+            <p className="text-center text-white bg-[#0fc9e6] rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">{formatRate.format(currentRate)}</p>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="flex-col h-48 items-end justify-center lg:h-auto mt-16">
+        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
+          <a href="https://vos.line-scdn.net/landpress-content-v2-ogautzqf79ax91l8h8icywfr/1705378299002.pdf">공식 토큰 교환비</a>는 <b>1KLAY(PDT) : {KLAY_TO_FNSA_RATIO}(FNSA)</b> 입니다
+        </div>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-gray-600"
+          href="https://bithumb.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
+          시세정보{' '}
+          <Image
+            src="/bithumb.png"
+            alt="Bithumb"
+            className="dark:invert"
+            width={100}
+            height={24}
+          />
         </a>
       </div>
     </main>
