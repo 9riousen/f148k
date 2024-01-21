cc8a444eea637e8a05bd0ee2c404758bd8f856dd
9riousen <9riousen@gmail.com>
2024-01-22 00:17:20 +0900

    가로정렬

--- app/page.tsx
+++ app/page.tsx
@@ -49,29 +49,31 @@ export default async function Home() {
         </div>
       </div>
 
+      <div className="flex flex-col content-center place-items-center p-1 text-gray-600">
+          <a
+            className="flex gap-2"
+            href="https://bithumb.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            시세정보{' '}
+            <Image
+              src="/bithumb.png"
+              alt="Bithumb"
+              className="dark:invert"
+              width={100}
+              height={24}
+            />
+          </a>
+        </div>
+
       <div className="flex-col h-48 items-end justify-center lg:h-auto mt-16">
         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
           <a href="https://zdnet.co.kr/view/?no=20240120014906">2024-01-20 | 클레이튼-핀시아 "통합 코인 PDT...교환 비율 개정안 마련할 것 | ZDNET Korea"</a>
         </div>
         <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
-          <a href="https://vos.line-scdn.net/landpress-content-v2-ogautzqf79ax91l8h8icywfr/1705378299002.pdf">2024-01-16 | 공식 토큰 교환비</a>는 <b>1KLAY(PDT) : {KLAY_TO_FNSA_RATIO}(FNSA)</b> 입니다
+          <a href="https://vos.line-scdn.net/landpress-content-v2-ogautzqf79ax91l8h8icywfr/1705378299002.pdf">2024-01-16 | 공식 토큰 교환비는 <b>1KLAY(PDT) : {KLAY_TO_FNSA_RATIO}(FNSA)</b> 입니다</a>
         </div>
-
-        <a
-          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 text-gray-600"
-          href="https://bithumb.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          시세정보{' '}
-          <Image
-            src="/bithumb.png"
-            alt="Bithumb"
-            className="dark:invert"
-            width={100}
-            height={24}
-          />
-        </a>
       </div>
     </main>
   )
