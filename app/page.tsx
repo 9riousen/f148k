82fdede3746787b1a67203f5edcdd4830c3c33a2
9riousen <9riousen@gmail.com>
2024-01-22 09:15:07 +0900

    반응형

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ export default async function Home() {
   const formatRate = new Intl.NumberFormat('ko-KR', { maximumFractionDigits: 3 })
   const LOGO_SIZE = 128;
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 bg-[#0fc9e6]">
-      <div className="relative flex place-items-center">
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-col items-center">
+    <main className="flex min-h-screen flex-col items-center p-4 bg-[#0fc9e6]">
+      <div className="relative flex flex-col lg:flex-row">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-row lg:flex-col items-center">
           <Image
             src='/klaytn.png'
             width={LOGO_SIZE}
@@ -35,7 +35,7 @@ export default async function Home() {
             <p className="text-center bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mt-2">1</p>
           </div>
         </div>
-        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-col items-center">
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white p-2 m-6 flex flex-row lg:flex-col items-center">
           <Image
             src='/finschia.png'
             width={LOGO_SIZE}
@@ -49,7 +49,7 @@ export default async function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col content-center place-items-center p-1 text-gray-600">
+      <div className="flex flex-col content-center p-1 text-gray-600">
           <a
             className="flex gap-2"
             href="https://bithumb.com"
@@ -68,10 +68,10 @@ export default async function Home() {
         </div>
 
       <div className="flex-col h-48 items-end justify-center lg:h-auto mt-16">
-        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
+        <div className="relative rounded-md px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
           <a href="https://zdnet.co.kr/view/?no=20240120014906">2024-01-20 | 클레이튼-핀시아 &quot;통합 코인 PDT...교환 비율 개정안 마련할 것&quot; | ZDNET Korea</a>
         </div>
-        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
+        <div className="relative rounded-md px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 mb-5">
           <a href="https://vos.line-scdn.net/landpress-content-v2-ogautzqf79ax91l8h8icywfr/1705378299002.pdf">2024-01-16 | 공식 토큰 교환비는 <b>1KLAY(PDT) : {KLAY_TO_FNSA_RATIO}(FNSA)</b> 입니다</a>
         </div>
       </div>
